64707d4646d2ba01f887a0ccf17907caa0e61216
Dave Jones <dave@waveform.org.uk>
2018-05-08 22:02:15 +0100

    Fix incorrect module doc-string

--- colorzero/easings.py
+++ colorzero/easings.py
@@ -27,7 +27,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-"Defines the main :class:`Color` class of the package."
+"Defines various easing functions for :meth:`Color.gradient`."
 
 from __future__ import (
     unicode_literals,
